e5fb6dc89a51e8ceca292c88da51c27c4c27b2d1
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2022-07-10 23:13:42 +0800

    完善renametools.py，规范文件格式

--- renametools.py
+++ renametools.py
@@ -12,12 +12,51 @@
     003-xxx.html
     
 手动修改这么多文件比较麻烦，所以通过写一个python的脚本来简化文件重名的过程
+
+os.rename(src , dst)
+
+获取当前目录
+import os
+os.getcwd()
+
+
+
 """
 
+from fileinput import filename
+import os
+
+print("rename tools started!")
+
 # 需要管理的文件夹
+cwdDir = os.getcwd()
 
+regWorkList = ['\\\\01.html\\\\case','\\\\02.css\\\\case','\\\\03.js\\\\case'] 
 # 遍历文件夹
-
-# 重命名文件
+# print(regWorkList)
+for dir in regWorkList:
+    # print(dir)
+    tmpDir = cwdDir+dir
+    # print(tmpDir)
+    
+    fileList = os.listdir(tmpDir)
+    for file in fileList:
+        fileName = os.path.splitext(file)[0]
+        fileSuffix = os.path.splitext(file)[1]
+        
+        newFileName = ""
+        # print(fileSuffix)
+        if fileSuffix != '.html':
+            continue
+        
+        
+        if fileName[2] == '-':
+            newFileName = '0'+fileName
+        else:
+            continue
+        
+        # print("filename:"+fileName)
+        os.rename(os.path.join(tmpDir,fileName+fileSuffix),os.path.join(tmpDir,newFileName+fileSuffix))
 
 # 打印结束的信息
+print("rename tools ended!")
